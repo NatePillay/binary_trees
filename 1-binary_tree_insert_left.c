30be75407362a98aa5179ce42d2afcaabe988f05
NatePillay <nathan94pillay@gmail.com>
2023-02-28 13:46:36 -0800

    new commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_insert_left - func to insert onto left of tree
+ * @parent: a pointer to the parent node
+ * @value: value returned at that position
+ * Return: binary_tree_t data type
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *leftnew;
+
+	leftnew = malloc(sizeof(binary_tree_t));
+
+	left->n = value;
+	new->parent = newnode;
+
+
+}
+
