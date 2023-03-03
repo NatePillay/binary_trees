b1a27ffcbcaa2d2ce16862e32e943b710809b54b
NatePillay <nathan94pillay@gmail.com>
2023-03-02 19:21:51 -0800

    new commit

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_is_full - func to check if binary tree is full
+ * @tree: func pointer to root
+ * Return: count or 0
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}	
